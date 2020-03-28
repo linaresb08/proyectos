3629f9d35ba49f9bcb129e725f84dc01f4b78ed5
Betzabeth Linares <linaresb08@gmail.com>
2020-03-28 13:13:14 -0400

    Probando los ejercicios de los primeros vídeos.

---
+++ CursoJS-practicas/Primeros Videos/js/codigo.js
@@ -0,0 +1,31 @@
+/*
+var edad = 17; // Es una variable global en js
+sumar();
+
+function sumar () {
+  alert(edad);
+  var edad = 19;
+}
+
+alert(edad);
+
+if (true) {
+  let edad = 19; // Es una variable declarada en Emac6
+  alert(edad);
+}
+alert(edad);
+
+const EDAD = 25;
+EDAD = 21;
+alert(EDAD);
+
+let a = 'Jab';
+let b = 7;
+let c = a + b;
+console.log(c);
+console.log(typeof(c));
+
+var jab;
+console.log(typeof(jab));
+
+*/
